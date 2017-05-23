f2f54266ec2fcf31c8f547447197e9a9e9ae7d38
julianilmenau <julian.mueller@student.hpi.de>
2017-05-23 10:43:11 +0200

    Update unionfind.cpp

---
+++ unionfind.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+struct node {
+	node *father;
+	int key;
+	int numNodes;
+	};
+	
+bool compare(const pair<int,pair<int,int>> &a,const pair<int,pair<int,int>> &b) {
+	return a.first>b.first;
+}
+	
+node makeSet(int key) {
+	node result = {NULL,key,1};
+	return result;
+}
+
+int find(node &pnode) {
+	node *temp=&pnode;
+	while (temp->father!=NULL) {
+		temp=temp->father;
+	}
+	if (!(pnode.father==NULL)) {
+		pnode.father=temp;
+	}
+	//cout<<"   is: "<<temp->key<<endl;
+	return temp->key;
+}
+
+void Union(node &x,node &y) {
+	y.father = &x;
+	y.numNodes+=x.numNodes;
+}
