ac3529033ecaae138031b4414bbb0372ac7a4f6f
marcpb <mpbismar@web.de>
2017-05-23 11:34:23 +0200

    minor changes

--- algorithms/Breitensuche.cpp
+++ algorithms/Breitensuche.cpp
@@ -5,7 +5,9 @@ void dfsvisit(vector<int> *graph,bool *visited,int i) {
 	*(visited+i)=1;
 	vector<int> adj=*(graph+i);
 	for (int j:adj) {
-		if (!(*(visited+j))) {				//NEEDS OUTPUT
+		if (!(*(visited+j))) {
+			//NEEDS OUTPUT OR SO
+			//i.e. nNodes++; or totalWeight += graph[i][j].weight or so:
 			dfsvisit(graph,visited,j);
 		}
 	}
