cde3023d13f47ca2ba1468cd1330617616e28760
marcpb <mpbismar@web.de>
2017-05-23 11:13:02 +0200

    restructuring

---
+++ utils/fastInput.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+void fastInput(int &number) //not thread safe
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar_unlocked();
+    if (c=='-')
+    {
+        negative = true;
+        c = getchar_unlocked();
+    }
+    for (; (c>47 && c<58); c=getchar_unlocked())
+        number = number *10 + c - 48;
+
+    if (negative)
+        number *= -1;
+}
+
+using namespace std;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int number = 0;
+    fastInput(number);
+    cout<<number;
+}
